3e2260237a709fc80eeafa171e8bdf0ac9e5a370
W0478410 <Kanishkmain@gmail.com>
2023-11-21 15:20:30 -0400

    GUI with ML guess

--- MLForm/Form1.cs
+++ MLForm/Form1.cs
@@ -1,3 +1,5 @@
+using System.Xml.Linq;
+using SentimentModel_ConsoleApp;
 namespace MLForm
 {
     public partial class Form1 : Form
@@ -9,7 +11,17 @@ namespace MLForm
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //get new data
+            var sampleData = new SentimentModel.ModelInput()
+            {
+                Col0 = textBox1.Text,
+            };
 
+            //Load model and predict output
+            var result = SentimentModel.Predict(sampleData);
+
+            guessBox.Text = result.PredictedLabel.ToString();
+            confidenceBox.Text = (result.Score[0] * 100).ToString();
         }
 
         private void button2_Click(object sender, EventArgs e)
